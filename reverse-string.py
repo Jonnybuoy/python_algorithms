05e10644718092fb6f0c9d49b80261ae29cf8abd
kaguithiajohnson@gmail.com <kaguithiajohnson@gmail.com>
2023-03-08 22:11:59 +0300

    Python algorithms practice

---
+++ reverse-string.py
@@ -0,0 +1,24 @@
+'''
+Reverse a string
+'''
+
+def reverse_string(s):
+    length = len(s)
+    spaces = [' ']
+    words = []
+    i = 0
+    
+    while i < length:
+        if s[i] not in spaces:
+            word_start = i
+            
+            while i < length and s[i] not in spaces:
+                i += 1
+            
+            words.append(s[word_start:i])
+            
+            i += 1
+    
+    return "".join(reversed(s))
+
+print(reverse_string("This is the best"))
